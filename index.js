3053a8a0188bc52e89681264f21088c03fa93984
gameel allam <gameel.allam@stealthium.io>
2024-07-16 20:03:47 +0300

    Inital commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyBpxfTX8mDwVCyZI90G4K2nZq8EmxIhPXQ",
+	authDomain: "test1-74285.firebaseapp.com",
+	projectId: "test1-74285",
+	storageBucket: "test1-74285.appspot.com",
+	messagingSenderId: "1031576965881",
+	appId: "1:1031576965881:web:438a5183cb0c46aac58828",
+};
